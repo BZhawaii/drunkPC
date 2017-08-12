f05fc95fe058438640127a25c476253914481a3f
Richard Beese <bzhawaii@yahoo.com>
2017-08-12 17:45:18 -0600

    Added some arguments

--- DrunkPC/Program.cs
+++ DrunkPC/Program.cs
@@ -25,6 +25,9 @@ namespace DrunkPC
         //      The _ is meant to indicate global
         public static Random _random = new Random();
 
+        public static int _statupDelaySeconds = 10;
+        public static int _totalDurationSeconds = 10;
+
         /// <summary>
         /// Entry point for prank application
         /// </summary>
@@ -33,12 +36,26 @@ namespace DrunkPC
         {
             Console.WriteLine("DrunkPC Prank Application by: BZ");
 
+            // Check for command line arguments and assign new values
+            if(args.Length >= 2)
+            {
+                _statupDelaySeconds = Convert.ToInt32(args[0]);
+                _totalDurationSeconds = Convert.ToInt32(args[1]);
+            }
+
             // Create all threads that manipulate all of the inputs and outputs
             Thread drunkMouseThread = new Thread(new ThreadStart(DrunkMouseThread));
             Thread drunkKeyboardThread = new Thread(new ThreadStart(DrunkKeyboardThread));
             Thread drunkSoundThread = new Thread(new ThreadStart(DrunkSoundThread));
             Thread drunkPopupThread = new Thread(new ThreadStart(DrunkPopupThread));
 
+            // Ends program 10 seconds after it starts
+            DateTime future = DateTime.Now.AddSeconds(_statupDelaySeconds);
+            Console.WriteLine("Waiting 10 seconds before starting threads");
+            while (future > DateTime.Now)
+            {
+                Thread.Sleep(1000);
+            }
 
             // Start all of the threads
             drunkMouseThread.Start();
@@ -46,8 +63,14 @@ namespace DrunkPC
             drunkSoundThread.Start();
             drunkPopupThread.Start();
 
-            // Wait for user input
-            Console.Read();
+            // Ends program 10 seconds after it starts
+            future = DateTime.Now.AddSeconds(_totalDurationSeconds);
+            while( future > DateTime.Now)
+            {
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Terminating all threads");
 
             // Kill all threads and exit application
             drunkMouseThread.Abort();
@@ -99,14 +122,18 @@ namespace DrunkPC
 
             while (true)
             {
-                // Generate a random capital letter
-                char key = (char)(_random.Next(25) + 65);
-
-                // 50/50 make it lower case
-                if(_random.Next(2) == 0)
+                if(_random.Next(100) > 80)
                 {
-                    key = Char.ToLower(key);
-                }
+                    // Generate a random capital letter
+                    char key = (char)(_random.Next(25) + 65);
+
+                    // 50/50 make it lower case
+                    if(_random.Next(2) == 0)
+                    {
+                        key = Char.ToLower(key);
+                    }  // closes if 2
+
+                } // closes if 100 >80
 
                 SendKeys.SendWait(key.ToString());
 
